4793ad3273d382ad1a247e9a1a18696d4f846ede
siham mohamud <sihammohamud744@gmail.com>
2026-04-02 17:36:02 +0300

    i am adding today variables, datatipes and functions

---
+++ fst/javascript/functions/functions.js
@@ -0,0 +1,135 @@
+// What girls like
+console.log("Girls like good communication");
+console.log("Girls like feeling appreciated");
+console.log("Girls like thoughtful gestures");
+console.log("Girls like honesty");
+console.log("Girls like emotional support");
+console.log("Girls like confidence");
+console.log("Girls like humor");
+console.log("Girls like kindness");
+console.log("Girls like respect");
+console.log("Girls like meaningful conversations");
+console.log("Girls like surprises");
+console.log("Girls like loyalty");
+console.log("Girls like compliments");
+console.log("Girls like creativity");
+console.log("Girls like ambition");
+console.log("Girls like being listened to");
+console.log("Girls like empathy");
+console.log("Girls like quality time");
+console.log("Girls like trust");
+console.log("Girls like consistency");
+
+// What boys like
+console.log("Boys like feeling respected");
+console.log("Boys like appreciation");
+console.log("Boys like fun activities");
+console.log("Boys like loyalty");
+console.log("Boys like humor");
+console.log("Boys like support");
+console.log("Boys like honesty");
+console.log("Boys like independence");
+console.log("Boys like confidence");
+console.log("Boys like compliments");
+console.log("Boys like shared interests");
+console.log("Boys like trust");
+console.log("Boys like physical affection");
+console.log("Boys like encouragement");
+console.log("Boys like clear communication");
+console.log("Boys like low drama");
+console.log("Boys like adventure");
+console.log("Boys like being understood");
+console.log("Boys like positivity");
+console.log("Boys like reliability");
+
+// if you print it as its it will not be seprated it will print it all so we neet to seprate it and we will use functions to saprate.
+
+
+// What girls like
+
+function WhatGirlsLike() {
+console.log("Girls like good communication");
+console.log("Girls like feeling appreciated");
+console.log("Girls like thoughtful gestures");
+console.log("Girls like honesty");
+console.log("Girls like emotional support");
+console.log("Girls like confidence");
+console.log("Girls like humor");
+console.log("Girls like kindness");
+console.log("Girls like respect");
+console.log("Girls like meaningful conversations");
+console.log("Girls like surprises");
+console.log("Girls like loyalty");
+console.log("Girls like compliments");
+console.log("Girls like creativity");
+console.log("Girls like ambition");
+console.log("Girls like being listened to");
+console.log("Girls like empathy");
+console.log("Girls like quality time");
+console.log("Girls like trust");
+console.log("Girls like consistency");
+}
+
+// What boys like
+
+function WhatBoysLike() {
+console.log("Boys like feeling respected");
+console.log("Boys like appreciation");
+console.log("Boys like fun activities");
+console.log("Boys like loyalty");
+console.log("Boys like humor");
+console.log("Boys like support");
+console.log("Boys like honesty");
+console.log("Boys like independence");
+console.log("Boys like confidence");
+console.log("Boys like compliments");
+console.log("Boys like shared interests");
+console.log("Boys like trust");
+console.log("Boys like physical affection");
+console.log("Boys like encouragement");
+console.log("Boys like clear communication");
+console.log("Boys like low drama");
+console.log("Boys like adventure");
+console.log("Boys like being understood");
+console.log("Boys like positivity");
+console.log("Boys like reliability");
+}
+
+// call a fuction it would no be ptinted without calling
+
+
+
+
+WhatGirlsLike()
+
+
+
+
+// we created at one time but we call it twice we just changing the value
+function multiply(a, b) {
+    return a * b;
+    }
+
+    var result = multiply(4, 5)
+    var result2 = multiply(3, 6)
+console.log(result);
+console.log(result2);
+
+function add(a, b) {
+    return a * b;
+    
+    
+}
+
+var result = add(10, 7);
+console.log(result);
+
+// another way to call a function
+
+var add = function (a, b)  {
+
+    return a + b;
+}
+
+var result = add(5, 7)
+console.log(result);
